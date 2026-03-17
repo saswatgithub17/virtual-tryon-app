70fc61db4bd85adce65cd556b06c89d2fdc7f58d
Saswat <saswatsumandwibedy17@gmail.com>
2026-03-17 16:37:08 +0530

    commiting

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -13,7 +13,7 @@ class ApiConfig {
   /// For Physical Device (WiFi) - USE YOUR COMPUTER'S IP ADDRESS
   /// Make sure to include the server port (default: 5000)
   /// To find your IP: Run 'ipconfig' on Windows or 'ifconfig' on Mac/Linux
-  static const String baseUrl = 'http://10.81.185.1/api';
+  static const String baseUrl = 'http://10.81.185.1:5000/api';
 
   /// For Localhost testing (only works on emulator/simulator)
   // static const String baseUrl = 'http://localhost:5000/api';
